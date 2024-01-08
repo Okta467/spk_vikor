608f1c053f0bf438ea03a726063fb55b66451d76
Okta Alfiansyah <oktaalfiansyah@gmail.com>
2024-01-09 04:16:10 +0700

    Add role Kepala Dusun

---
+++ application/controllers/kepala_dusun/Sub_Kriteria.php
@@ -0,0 +1,26 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Sub_kriteria extends CI_Controller {
+    const CURRENT_ACCESS_LEVEL = 'kepala_dusun';
+
+    public function __construct() {
+        parent::__construct();
+        // Redirect if user is not login or wrong access level
+        $this->load->model('m_auth');
+        if (!$this->m_auth->current_user() || !$this->m_auth->is_correct_access_level(self::CURRENT_ACCESS_LEVEL)) {
+            redirect('auth/login');
+        }
+
+        $this->load->model('m_kriteria');
+        $this->load->model('m_sub_kriteria');
+        $this->load->helper('formatting_validation_errors');
+    }
+
+	public function index() {
+		$data['kriterias']     = $this->m_kriteria->get_all();
+		$data['sub_kriterias'] = $this->m_sub_kriteria->get_join_all();
+
+		$this->load->view('kepala_dusun/v_sub_kriteria', $data);
+	}
+} 
